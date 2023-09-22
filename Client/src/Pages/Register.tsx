ed627a994a3d704b6665193c15271337c79997dc
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2023-09-22 06:04:22 +0530

    Ts Config changed

--- Client/src/Pages/Register.tsx
+++ Client/src/Pages/Register.tsx
@@ -37,7 +37,7 @@ const Register = (): ReactElement => {
 								name="email"
 								id="email"
 								className="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
-								placeholder="youremail.@domain.com"
+								placeholder="johndoe45@gmail.com"
 							/>
 						</div>
 						<div>
